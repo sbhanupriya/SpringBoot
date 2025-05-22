acd684b66f0173c8a71af2594215d7b2bd31f29b
sbhanupriya017 <sbhanupriya017@gmail.com>
2025-05-23 02:48:34 +0530

    add get user endpoint

--- src/main/java/com/example/springboot/controller/HomeController.java
+++ src/main/java/com/example/springboot/controller/HomeController.java
@@ -1,8 +1,10 @@
 package com.example.springboot.controller;
 
 
+import com.example.springboot.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +14,15 @@ public class HomeController {
     public String home(){
         return "Hello World";
     }
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public User user() {
+        User user = new User();
+        user.setId("1");
+        user.setName("Bhanu Priya");
+        user.setEmailId("xyz@gmail.com");
+        return user;
+    }
+
+
 }
