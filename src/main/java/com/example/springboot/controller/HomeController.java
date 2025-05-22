8c298ec50d1e446265aea12302666309ab203747
sbhanupriya017 <sbhanupriya017@gmail.com>
2025-05-23 01:10:36 +0530

    add home controller - default end point

---
+++ src/main/java/com/example/springboot/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World";
+    }
+}
