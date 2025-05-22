e55418c9d0537feb288f9ef7f27dac1f0b5862aa
sbhanupriya017 <sbhanupriya017@gmail.com>
2025-05-23 02:50:30 +0530

    change @RequestMapping method type Get to @GetMapping

--- src/main/java/com/example/springboot/controller/HomeController.java
+++ src/main/java/com/example/springboot/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.springboot.controller;
 
 import com.example.springboot.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class HomeController {
         return "Hello World";
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @GetMapping(value = "/user")
     public User user() {
         User user = new User();
         user.setId("1");
