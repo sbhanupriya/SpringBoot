b310cb6ad9915598e10a9a25e8718f5c583cb0dd
sbhanupriya017 <sbhanupriya017@gmail.com>
2025-05-24 00:57:25 +0530

    add post api to save employee + controller-service layers + interface-implementation layer

---
+++ src/main/java/com/example/springboot/model/Employee.java
@@ -0,0 +1,50 @@
+package com.example.springboot.model;
+
+public class Employee {
+    private String employeeId;
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String department;
+}
---
+++ src/main/java/com/example/springboot/service/EmployeeServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.springboot.service;
+
+import com.example.springboot.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    List<Employee> employeeList = new ArrayList<>();
+
+    @Override
+    public Employee save(Employee employee) {
+        if(employee.getEmployeeId()==null || employee.getEmployeeId().isEmpty()){
+            employee.setEmployeeId(UUID.randomUUID().toString());
+        }
+        employeeList.add(employee);
+        return employee;
+    }
+}
