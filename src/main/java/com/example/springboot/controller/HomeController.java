1547c0a995aee072a9ac2238282d837a780a6628
sbhanupriya017 <sbhanupriya017@gmail.com>
2025-05-24 00:17:24 +0530

    Add Path Variable - used for mandatory data field

--- src/main/java/com/example/springboot/controller/HomeController.java
+++ src/main/java/com/example/springboot/controller/HomeController.java
@@ -4,6 +4,7 @@ package com.example.springboot.controller;
 import com.example.springboot.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,9 @@ public class HomeController {
         user.setEmailId("xyz@gmail.com");
         return user;
     }
-
+    @GetMapping("/{id}/{id2}")
+    public String pathVariable(@PathVariable String id, @PathVariable("id2") String test){
+        return "The path variable is: " + id + " - " + test;
+    }
 
 }
